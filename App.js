5701e7a27c5e0ad74241ba7de13bff02ac2156b9
Sachin <ss0985211@gmail.com>
2024-09-04 09:57:05 +0530

    Created the base strucutre of the add goal app

--- App.js
+++ App.js
@@ -1,31 +1,121 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { useState, useref } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enterdGoals, setEnteredGoals] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const handleOnGoalChange = (e) => {
+    setEnteredGoals(e);
+  };
+
+  const handleOnAddGoal = () => {
+    setGoals((prev) => [...prev, enterdGoals]);
+    // setEnteredGoals("");
+  };
+
+  const handleOnDeleteGoal = (index) => {
+    const filterdGoals = goals.filter((val, id) => id !== index);
+    setGoals(filterdGoals);
+  };
+
+  const handleOnAllClear = () => {
+    setGoals([]);
+  };
   return (
-    <View style={styles.outerContainer}>
-      <Text style={styles.dummyText}>This is Outer Box</Text>
-      <Button title="Click Me" />
-      <View style={styles.innerContainer}>
-        <Text style={styles.dummyText}>This is inner Box</Text>
+    <View style={styles.mainContainer}>
+      {/* Text and buttons */}
+      <View style={styles.inputAndButtonContainer}>
+        <TextInput
+          onChangeText={handleOnGoalChange}
+          style={styles.textInput}
+          value={enterdGoals || ""}
+          placeholder="Enter Your Goal"
+        />
+        <Button
+          style={styles.addButton}
+          onPress={handleOnAddGoal}
+          title="Add Goals"
+        />
+      </View>
+
+      {/* Goals Here */}
+      <View style={styles.goalContainer}>
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <View key={itemData.index} style={styles.goalItem}>
+                <Text
+                  style={styles.goalText}
+                  onPress={() => handleOnDeleteGoal(itemData.index)}
+                >
+                  {itemData.item}
+                </Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+
+      <View>
+        <Button onPress={handleOnAllClear} title="clear" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  outerContainer: {
+  mainContainer: {
+    paddingTop: 60,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  inputAndButtonContainer: {
+    flex: 1,
+    padding: 5,
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
     alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+  },
+  textInput: {
+    width: "70%",
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    marginRight: 8,
+    padding: 8,
+  },
+  addButton: {
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "center",
+    textAlign: "center",
   },
-  innerContainer: {
-    marginTop: 10,
+
+  goalContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 6,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+
+  goalText: {
     color: "white",
-    backgroundColor: "pink",
-  },
-  dummyText: {
-    margin: 10,
-    padding: 10,
   },
 });
